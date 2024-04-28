0680a474e85bb8aacd49edb95999cd3c0ba3e65f
arjitsingh8271 <arjitkumarsingh@gmail.com>
2024-04-28 12:07:46 +0530

    update

--- char_in_str_cla.sh
+++ char_in_str_cla.sh
@@ -1,3 +1,14 @@
-char_count=$(echo -n "$1" | wc -m)
-echo "$char_count"
+char_count=`echo -n "$1" | wc -m`
+bytes_count=`echo -n "$1" | wc -c`
+word_count=`echo -n $1 | wc -w`
 
+echo "char count: $char_count"
+echo "bytes count: $bytes_count"
+echo "word count: $word_count"
+
+<<com
+OUTPUT: bash char_in_str_cla.sh "arjit singh"                                                                
+		char count: 11
+		bytes count: 11
+		word count: 2
+com
